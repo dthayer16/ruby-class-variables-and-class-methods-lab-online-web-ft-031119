1806e1016421680335c7d5d201fda31bcccf8cc3
David Thayer <dthayer16@gmail.com>
2019-03-24 00:31:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,12 +1,12 @@
 
 class Song
   attr_accessor :name, :artist, :genre
-  
+
   @@count = 0
   @@artists = []
   @@genres = []
-  
-  
+
+
   def initialize (name, artist, genre)
     @name = name
     @artist = artist
@@ -15,32 +15,32 @@ class Song
     @@artists << artist
     @@genres << genre
   end
-  
+
   def self.count
     @@count
   end
-  
+
   def self.artists
     @@artists.uniq
   end
-  
+
   def self.genres
     @@genres.uniq
   end
-  
+
   def self.genre_count
     hash = {}
-    
+
     @@genres.uniq.each {|genre|
       hash[genre] = @@genres.count(genre)
     }
-    
+
     hash
   end
-  
+
   def self.artist_count
     hash = {}
-    
+
     @@artists.uniq.each {|artist|
       hash[artist] = @@artists.count(artist)}
       hash
